c2920a2d6537a6c8f4ab2af360245b63addf2e11
Blaise143 <blaiseappol@gmail.com>
2025-06-11 12:50:48 +0200

    started unet implementation

--- models/unet.py
+++ models/unet.py
@@ -1,8 +1,33 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from typing import List
+from embedding import TimeEmbedding
+
+
+class ConvBlock(nn.Module):
+    def __init__(self,
+                 in_channels: int, out_channels: int,
+                 kernel_size: int, stride: int, normalization: nn.BatchNorm2d,
+                 activation: nn.ReLU = nn.ReLU()):
+        super().__init__()
+        self.convs = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                      kernel_size=kernel_size, stride=stride),
+            nn.BatchNorm2d(num_features=out_channels),
+            activation
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.convs(x)
 
 
 class UNET(nn.Module):
-    def __init__(channels):
+    def __init__(self, channels_list: List[int]):
+        super().__init__()
+
+        encoder = nn.ModuleList()
+
+    @classmethod
+    def sinusoidal_embedding(t: Union[float, torch.Tensor]):
         ...
