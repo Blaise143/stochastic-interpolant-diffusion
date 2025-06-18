69b12b2b2d0e83115c01e78bdb4c6eb5cb9dd2b1
Blaise143 <blaiseappol@gmail.com>
2025-06-18 21:22:51 +0200

    increased num epochs and num steps

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from models import Diffusion
 from sampling import EulerMaruyamaSolver
 from training import train
 if __name__ == "__main__":
-    train(num_steps=500, epochs=50)
+    train(num_steps=1000, epochs=1000)
