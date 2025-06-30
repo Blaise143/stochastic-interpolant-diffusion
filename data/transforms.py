0e6080a276f79fbc87518f66c7dd8a07393a6e0a
Blaise143 <blaiseappol@gmail.com>
2025-06-30 22:02:24 +0200

    undone rotation done my emnist

--- data/transforms.py
+++ data/transforms.py
@@ -3,6 +3,8 @@ import torchvision
 
 def emnist_transform():
     return torchvision.transforms.Compose([
+        transforms.Lambda(lambda x: transforms.functional.rotate(x, -90)),
+        transforms.Lambda(lambda x: transforms.functional.hflip(x)),
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize((0.5,), (0.5,))
     ])
