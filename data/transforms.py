fd69adc90840e2713c16b7237be7611ae618a558
Blaise143 <blaiseappol@gmail.com>
2025-06-30 22:05:55 +0200

    fixed bug

--- data/transforms.py
+++ data/transforms.py
@@ -3,8 +3,10 @@ import torchvision
 
 def emnist_transform():
     return torchvision.transforms.Compose([
-        transforms.Lambda(lambda x: transforms.functional.rotate(x, -90)),
-        transforms.Lambda(lambda x: transforms.functional.hflip(x)),
+        torchvision.transforms.Lambda(
+            lambda x: transforms.functional.rotate(x, -90)),
+        torchvision.transforms.Lambda(
+            lambda x: transforms.functional.hflip(x)),
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize((0.5,), (0.5,))
     ])
