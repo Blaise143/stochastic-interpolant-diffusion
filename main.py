63be3c563d78a7b0a485cafe3f35941ff947797f
Blaise143 <blaiseappol@gmail.com>
2025-06-30 18:52:30 +0200

    train baby

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import torch
 import torch.nn as nn
+from training.flow_trainer import train, train_one_example
 
-from models import Diffusion
-from sampling import EulerMaruyamaSolver
-from training import train, train_one_example
 if __name__ == "__main__":
-    num_steps, num_epochs = 1000, 500
-    epochs = num_epochs*num_epochs
-    train(num_steps=num_steps, epochs=epochs, batch_size=200)
-
-    # train_one_example(num_steps=num_steps, epochs=epochs, lr=3e-4)
+    train(epochs=1000)
