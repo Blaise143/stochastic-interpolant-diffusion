4fbe4b2dea14fd4152b52662161aa69fd2a98b65
Blaise143 <blaiseappol@gmail.com>
2025-06-30 18:53:36 +0200

    removed sinusoidal, now only fourrier encodings

--- models/diffusion.py
+++ models/diffusion.py
@@ -10,13 +10,11 @@ class Diffusion(StochasticInterpolant):
     def __init__(self, model: Unet = Unet(in_channels=1,
                                           out_channels=1,
                                           channels_list=[32, 64, 128],
-                                          embedding_dim=100,
-                                          use_sinusoidal=False)):
+                                          embedding_dim=100)):
         super().__init__()
 
         self.eps_predictor = model
         self.criterion = nn.MSELoss()
-        self.bce = nn.BCEWithLogitsLoss()
 
     def sample(self, x: torch.Tensor):
         batch_size = x.shape[0]
@@ -32,7 +30,6 @@ class Diffusion(StochasticInterpolant):
     def compute_loss(self, x: torch.Tensor):
         xt, t, eps = self.sample(x)
         eps_pred = self.eps_predictor(xt, t)
-        # loss = self.criterion(eps, eps_pred)
         loss = self.criterion(eps_pred, eps)
         return loss
 
