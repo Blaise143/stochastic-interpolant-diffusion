a646d72666f279e75727d45eb9ebcba642cf66ff
Blaise143 <blaiseappol@gmail.com>
2025-06-30 18:53:14 +0200

    fixed transform. now range of input is [-1,1]

---
+++ data/transforms.py
@@ -0,0 +1,8 @@
+import torchvision
+
+
+def emnist_transform():
+    return torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5,), (0.5,))
+    ])
